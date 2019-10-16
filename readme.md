674a8321e9bb011886134386ffd9babc87da38d2
Orta Therox <orta.therox@gmail.com>
2019-10-15 22:10:59 -0400

    Wrap it up mostly

--- readme.md
+++ readme.md
@@ -1,22 +1,66 @@
 # ts-fourslash-create CLI
 
-A CLI for ts-fourslash-create.
+A CLI for creating a fourslash test file for the TypeScript compiler based on a repro folder.
 
-## Customizing your CLI
+### Usage
 
-Check out the documentation at https://github.com/infinitered/gluegun/tree/master/docs.
+```sh
+$ npx ts-fourslash-create [optional path]
 
-## Publishing to NPM
+/// <reference path="fourslash.ts" />
 
-To package your CLI up for NPM, do this:
+// @target: es5
+// @module: commonjs
+// @allowJs: true
+// @checkJs: true
+// @esModuleInterop: true
 
-```shell
-$ npm login
-$ npm whoami
-$ npm lint
-$ npm test
-(if typescript, run `npm run build` here)
-$ npm publish
+// @Filename: ex.js
+////import foo from './other'
+////// const a =
+////foo.defaults = 23
+////
+////
+////// const a = require("axios")
+////// a.default.defaults.timeout = 22
+////
+
+// @Filename: other.js
+////export default {
+////  defaults: "hello"
+////}
+////
+
+// @Filename: src/thing.ts
+////enum Rating {
+////  Like = 1,
+////  Dislike
+////};
+////
+////let a: Rating;
+////
+////switch (a) {
+////  case Rating.Like:
+////    break;
+////
+////  case Rating.Dislike:
+////    break;
+////
+////  default:
+////    let s: never = a;
+////}
+```
+
+### Direct to pasteboard (on os x)
+
+```sh
+npx ts-fourslash-create repros/23211 | pbcopy
+```
+
+### Direct to a file
+
+```sh
+npx ts-fourslash-create > testFile.ts
 ```
 
 # License
--- src/commands/ts-fourslash-create.ts
+++ src/commands/ts-fourslash-create.ts
@@ -1,22 +1,83 @@
-
 import { GluegunToolbox, GluegunCommand } from 'gluegun'
 import { join } from 'path'
+import * as commonPath from 'common-path'
+import * as ts from '@orta/language-services'
 
 const create: GluegunCommand = {
   name: 'ts-fourslash-create',
-  description: "",
+  description: `Converts a repro folder for a TS bug into a fourslash test.
+
+Usage: npx ts-fourslash-create [optional path to folder]
+  `,
   run: async (toolbox: GluegunToolbox) => {
-    const { print, filesystem: fs } = toolbox
+    const { filesystem: fs } = toolbox
 
-    const dir = toolbox.parameters.first ? toolbox.parameters.first : "."
+    const dir = toolbox.parameters.first ? toolbox.parameters.first : '.'
     const folder = join(fs.cwd(), dir)
-    
-    print.debug(`Looking at ${folder}`)
-    // print.info('Welcome to your CLI, looking')
 
-    fs.dir(folder)
+    const addFiles = (paths: string[], prefix: string) => {
+      let subpaths = []
+      paths.forEach(p => {
+        const path = join(prefix, p)
+        if (fs.isDirectory(path)) {
+          subpaths = [...subpaths, ...addFiles(fs.list(path), path)]
+        } else {
+          subpaths.push(fs.path(join(prefix, p)))
+        }
+      })
+      return subpaths
+    }
 
-  },
+    const denyListFiles = (files: string[]) => {
+      return files.filter(
+        f =>
+          !f.includes('node_modules/') &&
+          !f.includes('yarn.') &&
+          !f.includes('package-lock') &&
+          !f.includes('package.json')
+      )
+    }
+
+    // Recursively grab all the files in the repro folder
+    const rootFiles = fs.list(folder)
+    const allFiles = denyListFiles(addFiles(rootFiles, folder))
+
+    const commonPaths = commonPath(allFiles)
+    const commonPathsRelativeToCommonRoot = commonPaths.parsedPaths.map(p => join(p.subPart, p.basePart))
+
+    // Try get the tsconfig settings
+    let config = {}
+    const tsconfig = allFiles.find(p => p.endsWith('tsconfig.json'))
+    if (tsconfig) {
+      const foundConfig = JSON.parse(fs.read(tsconfig))
+      const foundCompilerSettings = foundConfig.compilerOptions || {}
+
+      // This is a bit meh of a technique, given that the function returns
+      // a very simple object
+      const defaults = ts.getDefaultCompilerOptions()
+
+      const keys = Object.keys(foundCompilerSettings)
+      keys.forEach(compilerKey => {
+        if (foundCompilerSettings[compilerKey] !== defaults[compilerKey]) {
+          config[compilerKey] = foundCompilerSettings[compilerKey]
+        }
+      })
+    }
+
+    // Make a list of files
+    const compilerSettings = Object.keys(config).map(k => `// @${k}: ${config[k]}`)
+    const files = commonPathsRelativeToCommonRoot.filter(f => !f.endsWith("tsconfig.json")).map(f => {
+      const file = fs.read(join(commonPaths.commonDir, f))
+      return `// @Filename: ${f}\n////` + file.split("\n").join("\n////")
+    })
+
+    // Print the file for you
+    const theFile = `/// <reference path="fourslash.ts" />
+${compilerSettings.length ? "\n" + compilerSettings.join("\n") + "\n" : ""}
+${files.join("\n\n")}
+    `
+    console.log(theFile)
+  }
 }
 
 module.exports = create
